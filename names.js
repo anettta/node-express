83d0c2d60c82351ebb2e6601b4f80f63a4de0991
Anya <annagapyuk@gmail.com>
2021-03-16 17:52:59 -0400

    node-express

---
+++ names.js
@@ -0,0 +1,16 @@
+// local
+const secret = 'SUPER SECRET'
+// share
+const john = 'john'
+const peter = 'peter'
+
+
+module.exports = {john,peter}
+
+module.exports.items = ['item1', 'item2']
+
+const person = {
+    name: 'bob',
+}
+
+module.exports.singlePerson = person
