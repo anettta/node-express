77db602841edbccad1667c981f0cc74aa996426c
Anya <annagapyuk@gmail.com>
2021-03-18 19:37:19 -0400

    added event loop js

--- app.js
+++ app.js
@@ -13,11 +13,43 @@
 // npm init (step by step)
 // npm init -y (everything default)
 
-const _ = require('lodash');
+// const _ = require('lodash');
 
-const items = [1,[2,[3,[4]]]]
+// const items = [1,[2,[3,[4]]]]
 
-const newItems = _.flattenDeep(items);
+// const newItems = _.flattenDeep(items);
 
-console.log(newItems);
+// console.log(newItems);
 
+// console.log('hello people');
+
+const {readFile} = require('fs');
+
+const getText = (path) => {
+    return new Promise((resolve, reject)=>{
+        readFile(path, 'utf8', (err, data) => {
+    if (err) {
+        reject(err)
+    } else {
+        resolve(data)
+    }
+    })
+  })
+}
+
+
+const start = async()=>{
+    try {
+        const first = await getText('./content/first.txt')
+         const second = await getText('./content/second.txt')
+   console.log(first, second)
+} catch (error) {
+    console.log(error)
+}
+}
+
+start()
+
+// getText('./content/first.txt')
+// .then((result) => console.log(result))
+// .catch((err) => console.log(err))
